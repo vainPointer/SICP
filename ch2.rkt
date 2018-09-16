ac11150c5d704fc5d16ad277ac45b9bad0c848a9
vainPointer <vainPointer@aliyun.com>
2018-09-16 20:25:11 +0800

    ch2

--- ch2.rkt
+++ ch2.rkt
@@ -307,4 +307,91 @@
 ; nil
 
 ; Section 2.3
-; Ex 2.53
+; Ex 2.54
+(define (memq item seq)
+  (cond ((null? seq) false)
+        ((eq? item (car seq)) seq)
+        (else (memq item (cdr seq)))))
+
+(define (equal? a b)
+  (cond ((and (not (pair? a)) (not (pair? b))) (eq? a b))
+        ((and (pair? a) (pair? b)) (and (equal? (car a) (car b)) (equal? (cdr a) (cdr b))))
+        (else false)))
+; Ex 2.55
+(quote (quote a))
+
+; Section 2.3.2 符号求导
+; Ex 2.56
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+	((exponentiation? exp)
+	 (make-product
+	  (make-product
+	   (exponent exp)
+	   (make-exponentiation
+	    (base exp)
+	    (make-sum (exponent exp) -1)))
+	  (deriv (base exp) var)))
+        (else (error "unknown expression type -- DERIV" exp))))
+
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2) (+ a1 a2)))
+        (else (list '+ a1 a2))))
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0) 0))
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+(define (addend s) (cadr s))
+(define (augend s) (caddr s))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+(deriv '(* x y) 'x)
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+(define (base x) (cadr x))
+(define (exponent x) (caddr x))
+(define (make-exponentiation a b)
+  (cond ((=number? a 1) 1)
+	((=number? a 0) 0)
+	((=number? b 0) 1)
+	((=number? b 1) a)
+	((and (number? a) (number? b) (expt a b)))
+	(else (list '** a b))))
+
+; Ex 2.57
+(define (augend2 x)
+  (cond ((= (length x) 3) (caddr x))
+	(else (cons '+ (cddr x)))))
+
+(define (multiplicand2 x)
+  (cond ((= (length x) 3) (caddr x))
+	(else (cons '* (cddr x)))))
+
